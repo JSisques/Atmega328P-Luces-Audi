6bc51d142df660182d966db42a7c9bceb91c49e2
Javi <javierplazasisques@gmail.com>
2021-03-26 21:01:00 +0100

    Root commit

--- main.asm
+++ main.asm
@@ -4,24 +4,24 @@
 
 ********************************************************************/	
 
-SER R16
-OUT DDRD, R16
+SER R16																;Inicializamos R16 con todo unos
+OUT DDRD, R16														;Establecemos todos los pines del puerto D como salida
 
 main:
-	CLR R16
-	OUT PORTD, R16
-	CALL delay_100
+	CLR R16															;Guardamos todo ceros en R16
+	OUT PORTD, R16													;Sacamos por los puertos el valor de R16
+	CALL delay_100													;Llamamos a la función delay de 100ms
 	bucle:
-		LSL R16
-		INC R16
-		OUT PORTD, R16
-		CALL delay_100
-		CPI R16, 255
-		BRNE bucle
+		LSL R16														;Hacemos un desplazamiento hacia la izquierda de R16
+		INC R16														;Incrementamos R16 en 1
+		OUT PORTD, R16												;Sacamos por los puertos D el valor de R16
+		CALL delay_100												;Llamamos a la función delay de 100ms
+		CPI R16, 255												;Comparamos si R16 es igual a 255
+		BRNE bucle													;Si lo anterior no es igual saltamos a la etiqueta bucle
 
-	OUT PORTD, R16
-	CALL delay_1
-	RJMP main
+	OUT PORTD, R16													;Sacamos por los puertos D el valor de R16
+	CALL delay_1													;Llamamos a la función delay de 1 segundo
+	RJMP main														;Hacemos un salto relativo a la etiqueta main
 
 	
 
@@ -35,9 +35,9 @@ main:
 
 delay_100:
 
-	PUSH R20									;Copia de seguridad del registro 20
-	PUSH R21									;Copia de seguridad del registro 21
-	PUSH R22									;Copia de seguridad del registro 22
+	PUSH R20														;Copia de seguridad del registro 20
+	PUSH R21														;Copia de seguridad del registro 21
+	PUSH R22														;Copia de seguridad del registro 22
 
 	; Assembly code auto-generated
 	; by utility from Bret Mulvey
@@ -55,15 +55,15 @@ delay_100:
 		brne L1
 		nop
 	
-	POP R22										;Restauración del registro 22
-	POP R21										;Restauración del registro 21
-	POP R20										;Restauración del registro 20
-	RET											;RET es el equivalente al return, antes deberemos de haber guardado el PC (Program Counter)	
+	POP R22															;Restauración del registro 22
+	POP R21															;Restauración del registro 21
+	POP R20															;Restauración del registro 20
+	RET																;RET es el equivalente al return, antes deberemos de haber guardado el PC (Program Counter)	
 
 delay_1:
-	PUSH R20									;Copia de seguridad del registro 20
-	PUSH R21									;Copia de seguridad del registro 21
-	PUSH R22									;Copia de seguridad del registro 22
+	PUSH R20														;Copia de seguridad del registro 20
+	PUSH R21														;Copia de seguridad del registro 21
+	PUSH R22														;Copia de seguridad del registro 22
 
 	
 	; Assembly code auto-generated
@@ -85,7 +85,7 @@ delay_1:
 		nop
 
 
-	POP R22										;Restauración del registro 22
-	POP R21										;Restauración del registro 21
-	POP R20										;Restauración del registro 20
-	RET											;RET es el equivalente al return, antes deberemos de haber guardado el PC (Program Counter)	
+	POP R22															;Restauración del registro 22
+	POP R21															;Restauración del registro 21
+	POP R20															;Restauración del registro 20
+	RET																;RET es el equivalente al return, antes deberemos de haber guardado el PC (Program Counter)	
